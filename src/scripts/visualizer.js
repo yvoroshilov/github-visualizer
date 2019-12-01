63b2809cd8cea02b5b0ae2a106bbd3250e3b1c42
Voroshilov <saynorandvander@gmail.com>
2019-12-01 17:49:26 +0300

    fixed previous bug, added comparator instead of anon function

--- src/scripts/visualizer.js
+++ src/scripts/visualizer.js
@@ -6,13 +6,7 @@ async function visualize () {
 }
 
 function setLevels () {
-    allUniqueCommits.sort(function (a, b) {
-        let dateA = new Date(a.commit.committer.date);
-        let dateB = new Date(b.commit.committer.date);
-        if (dateA < dateB) return -1;
-        if (dateA > dateB) return 1;
-        if (dateA === dateB) return 0;
-    });
+    allUniqueCommits.sort(comparator);
     for (let i = 0; i < allUniqueCommits.length; i++) {
         levels[i] = -1;
     }
@@ -32,18 +26,31 @@ function setLevels () {
             curLevel = Math.max(curLevel, levels[i]);
         }
         curLevel++;
+        if ("children" in curParent && curParent.children.length > 1) {
+            let x = curParent.children.length - curParent.children.indexOf(curParent.children.find(x => x[0].sha === curChain[0].sha));
+            curLevel = Math.max(curLevel, levels[begin] + x);
+        }
 
         for (let i = curChain.length - 1; i >= 0; i--) {
             let index = allUniqueCommits.indexOf(allUniqueCommits.find(x => x.sha === curChain[i].sha));
             levels[index] = curLevel;
 
             if ("children" in curChain[i]) {
-                for (let j = 0; j < curChain[i].children.length; j++) {
+                for (let j = curChain[i].children.length - 1; j >= 0 ; j--) {
                     queue.push({chain: curChain[i].children[j], parent: curChain[i]});
                 }
             }
         }
     }
+
+    function comparator (a, b) {
+        let dateA = new Date(a.commit.committer.date);
+        let dateB = new Date(b.commit.committer.date);
+        if (dateA < dateB) return -1;
+        if (dateA > dateB) return 1;
+        if (dateA === dateB) return 0;
+    }
+
     console.log(levels);
 
 }
